136a139bfc4082f164edbe9bb7b508e591bb7573
Michael <onisoris@yandex.ru>
2024-03-12 00:56:49 +0300

    Функция для проверки позиции двух прямых относительно друг друга

---
+++ tests/test_functions/check_position_lines.py
@@ -0,0 +1,20 @@
+from threeDTool import *
+from line import Line
+
+# line1 = Line(1, 0, 0, 1, 0, 0)
+# line2 = Line(0, 1, 0, 1, 0, 0)
+# # Компланарные параллельные векторы
+# # Пройдено
+# logger.debug(check_position_lines(line1, line2))
+
+line1 = Line(1, 0, 0, 1, 0, 0)
+line2 = Line(0, 1, 0, 1, 2, 0)
+# Компланарные параллельные векторы
+# Пройдено
+logger.debug(check_position_lines(line1, line2))
+
+line1 = Line(1, 0, 0, 1, 0, 1)
+line2 = Line(0, 1, 0, 1, 2, 0)
+# Некомпланарные векторы
+# Пройдено
+logger.debug(check_position_lines(line1, line2))
