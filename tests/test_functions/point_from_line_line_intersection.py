4c02d3d459808a1d0dfe7ffc0fc3eaf4d3f9f88d
Michael <onisoris@yandex.ru>
2024-03-13 16:30:15 +0300

    Проверка функции point_from_line_line_intersection

---
+++ tests/test_functions/point_from_line_line_intersection.py
@@ -0,0 +1,26 @@
+from threeDTool import *
+from line import Line
+
+line1 = Line(2, 0, 0, 0, 1, 0)
+line2 = Line(0, 2, 0, 1, 0, 0)
+point = point_from_line_line_intersection(line1, line2)
+# Проверка пройдена (вернет 2 2 0)
+logger.debug(point)
+
+line1 = Line(2, 0, 0, 1, 0, 0)
+line2 = Line(0, 2, 0, 1, 0, 0)
+point = point_from_line_line_intersection(line1, line2)
+# Проверка пройдена, вернет None
+logger.debug(point)
+
+line1 = Line(0, 0, 0, 1, 0, 1)
+line2 = Line(0, 0, 0, 1, 0, 0)
+point = point_from_line_line_intersection(line1, line2)
+# Проверка пройдена, вернет (0, 0, 0)
+logger.debug(point)
+
+line1 = Line(1, 1, 1, 1, 1, 1)
+line2 = Line(1, 1, 1, 0, 1, 0)
+point = point_from_line_line_intersection(line1, line2)
+# Проверка пройдена, вернет (1, 1, 1)
+logger.debug(point)
