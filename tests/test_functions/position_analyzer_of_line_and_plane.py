c0e597f3f76e214f67ae04878a3e384e67397704
Michael <onisoris@yandex.ru>
2024-03-13 16:33:42 +0300

    Проверка функции position_analyzer_of_line_and_plane

--- tests/test_functions/position_analyzer_of_line_and_plane.py
+++ tests/test_functions/position_analyzer_of_line_and_plane.py
@@ -1,9 +1,15 @@
 from line import Line
 from plane import Plane
 from threeDTool import position_analyzer_of_line_and_plane
-# Линия, параллельная оси y
+# Линия, параллельная оси y, лежащая в плоскости xy
 line = Line(1, 0, 0, 0, 1, 1)
 # Плоскость в осях x, y
 plane = Plane(0, 0, 1, 0)
+# Тест пройден, вернуло 0
+print(position_analyzer_of_line_and_plane(line, plane))
 
-position_analyzer_of_line_and_plane(line, plane)
+line = Line(1, 0, 0, 1, 1, 1)
+# Плоскость в осях x, y
+plane = Plane(0, 0, 1, 0)
+# Тест пройден, вернуло 0
+print(position_analyzer_of_line_and_plane(line, plane))
