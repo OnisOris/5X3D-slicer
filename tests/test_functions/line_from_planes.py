d40bc74962ac074035de1fdd01216173c8e317cd
Michael <onisoris@yandex.ru>
2024-03-13 16:33:07 +0300

    Проверка функции line_from_planes класса Line

---
+++ tests/test_functions/line_from_planes.py
@@ -0,0 +1,19 @@
+from threeDTool import *
+from line import Line
+from plane import Plane
+
+plane1 = Plane(0, 0, 1, 0)
+plane2 = Plane(1, 0, 0, 0)
+
+line = Line()
+line.line_from_planes(plane1, plane2)
+# Тест пройден
+line.info()
+
+plane1 = Plane(0, 0, 1, 0)
+plane2 = Plane(1, 1, 0, 0)
+
+line = Line()
+line.line_from_planes(plane1, plane2)
+# Тест пройден
+line.info()
