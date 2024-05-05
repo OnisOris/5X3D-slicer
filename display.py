f493fe450c9fce043a5b488a0a691b475e6d56b3
Michael <onisoris@yandex.ru>
2024-05-05 12:28:56 +0300

    Класс отображения объектов из приложения

--- display.py
+++ display.py
@@ -7,19 +7,22 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 # mpl.use('TkAgg')
 
-mpl.use('Qt5Agg')
 class Dspl:
-    def __init__(self, input_array):
+    def __init__(self, input_array, qt=True):
+        if qt:
+            mpl.use('Qt5Agg')
         self.input_array = input_array
         self.fig = None
         self.ax = None
-        # self.create_subplot3D()
+        self.create_subplot3D()
         # input_array[0].show(self.ax)
 
     def create_subplot3D(self):
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, projection='3d')
-        logger.debug("fg")
+        self.ax.set_xlabel('X Label')
+        self.ax.set_ylabel('Y Label')
+        self.ax.set_zlabel('Z Label')
     def show(self):
         for obj in self.input_array:
             obj.show(self.ax)
