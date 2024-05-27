edfd998017d0090e413d5870ac6289df1da135b7
Michael <onisoris@yandex.ru>
2024-05-27 22:10:21 +0300

    Описание параметров Денавита-Хантерберга в отдельном словаре и файле

---
+++ DH_config.py
@@ -0,0 +1,15 @@
+import numpy as np
+from numpy import pi
+
+DH = {
+    'a_1': 0,
+    'a_2': 0,
+    'alpha_1': pi / 2,
+    'alpha_2': -pi / 2,
+    'd_1': 0,
+    'd_2': 10,
+    'displacement_theta_1': 0,
+    'displacement_theta_2': 0,
+}
+
+
