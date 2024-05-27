d30ffa35f275474daf5b31f8258b599ab95b0a7a
Michael <onisoris@yandex.ru>
2024-05-27 22:19:00 +0300

    Изменение приватных полей на защищенные в Line, чтобы в отрезок можно было без проблем наследовать поля линии. В Line_segment Добавлены поля для характеристики для оторбражения цвета и размера линии. Добавлена функция show. Удалены гетеры и сеттеры, так как идет нормальное наследование защищенных полей

--- line.py
+++ line.py
@@ -13,68 +13,68 @@ class Line:
     # Уравнение вида:
     # (x-a)/p1 = (y-b)/p2 = (z-c)/p3
     def __init__(self, a=0, b=0, c=0, p1=1, p2=0, p3=0, logging=False):
-        self.__a = a
-        self.__b = b
-        self.__c = c
-        self.__p1 = p1
-        self.__p2 = p2
-        self.__p3 = p3
+        self._a = a
+        self._b = b
+        self._c = c
+        self._p1 = p1
+        self._p2 = p2
+        self._p3 = p3
         self.logging = logging
 
     @property
     def a(self):
-        return self.__a
+        return self._a
 
     @property
     def b(self):
-        return self.__b
+        return self._b
 
     @property
     def c(self):
-        return self.__c
+        return self._c
 
     @property
     def p1(self):
-        return self.__p1
+        return self._p1
 
     @property
     def p2(self):
-        return self.__p2
+        return self._p2
 
     @property
     def p3(self):
-        return self.__p3
+        return self._p3
 
     @a.setter
     def a(self, a):
-        self.__a = a
+        self._a = a
 
     @b.setter
     def b(self, b):
-        self.__b = b
+        self._b = b
 
     @c.setter
     def c(self, c):
-        self.__c = c
+        self._c = c
 
     @p1.setter
     def p1(self, p1):
-        self.__p1 = p1
+        self._p1 = p1
 
     @p2.setter
     def p2(self, p2):
-        self.__p2 = p2
+        self._p2 = p2
 
     @p3.setter
     def p3(self, p3):
-        self.__p3 = p3
+        self._p3 = p3
 
     def info(self) -> None:
         logger.debug(
-            f'a = {self.__a}, b = {self.__b}, c = {self.__c}, p1 = {self.__p1}, p2 = {self.__p2}, p3 = {self.__p3}')
+            f'a = {self._a}, b = {self._b}, c = {self._c}, p1 = {self._p1}, p2 = {self._p2}, p3 = {self._p3}')
 
     def coeffs(self) -> ndarray:
-        return np.array([self.__a, self.__b, self.__c, self.__p1, self.__p2, self.__p3])
+        return np.array([self._a, self._b, self._c, self._p1, self._p2, self._p3])
 
     # Создание коэффициентов прямой по двум точкам в пространстве.
     # Принимает точку в виде массива 1x3 объекта класса numpy.array с тремя координатами [x, y, z]
@@ -88,13 +88,13 @@ class Line:
         :return: None
         """
         if np.shape(point1)[0] == 2:
-            self.__c = 0
+            self._c = 0
             p3 = 0
         else:
-            self.__c = point1[2]
+            self._c = point1[2]
             p3 = point2[2] - point1[2]
-        self.__a = point1[0]
-        self.__b = point1[1]
+        self._a = point1[0]
+        self._b = point1[1]
 
         # TODO: проверка на нуль
         p1 = point2[0] - point1[0]
@@ -110,9 +110,9 @@ class Line:
                 p1 = p1 / mod_N
                 p2 = p2 / mod_N
                 p3 = p3 / mod_N
-            self.__p1 = p1
-            self.__p2 = p2
-            self.__p3 = p3
+            self._p1 = p1
+            self._p2 = p2
+            self._p3 = p3
 
     def line_from_planes(self, plane1, plane2):
         '''
@@ -137,9 +137,9 @@ class Line:
             elif mod_p == 0:
                 logger.error("P - нулевой вектор")
                 return True
-            self.__p1 = p1
-            self.__p2 = p2
-            self.__p3 = p3
+            self._p1 = p1
+            self._p2 = p2
+            self._p3 = p3
             # как найти точку с прямой?
             # если прямая не параллельна x и y, мы можем занулить координату z (c = 0)
             # Но если она параллельна, то одна из плоскостей plane1 или plane 2 параллельна осям x и y, тогда необходимо
@@ -158,32 +158,32 @@ class Line:
             val3_1 = plane2.c * plane1.b - plane1.c * plane2.b
             val3_2 = plane2.c * plane1.b - plane1.c * plane2.b
             if val1_1 != 0 and plane2.b != 0:
-                self.__c = 0
-                self.__a = (plane2.d * plane1.b - plane1.d * plane2.b) / val1_1
-                self.__b = - (plane2.a * self.__a + plane2.d) / plane2.b
+                self._c = 0
+                self._a = (plane2.d * plane1.b - plane1.d * plane2.b) / val1_1
+                self._b = - (plane2.a * self._a + plane2.d) / plane2.b
             elif val1_2 != 0 and plane2.b == 0:
-                self.__c = 0
-                self.__b = (plane1.a * plane2.d - plane1.d * plane2.a) / val1_2
-                self.__a = - (plane2.b * self.__c + plane2.d) / plane2.a
+                self._c = 0
+                self._b = (plane1.a * plane2.d - plane1.d * plane2.a) / val1_2
+                self._a = - (plane2.b * self._c + plane2.d) / plane2.a
 
             elif val2_1 != 0 and plane2.c != 0:
-                self.__b = 0
-                self.__a = (plane2.d * plane1.c - plane1.d * plane2.c) / val2_1
-                self.__c = -(plane2.a * self.__a + plane2.d) / plane2.c
+                self._b = 0
+                self._a = (plane2.d * plane1.c - plane1.d * plane2.c) / val2_1
+                self._c = -(plane2.a * self._a + plane2.d) / plane2.c
 
             elif val2_2 != 0 and plane2.a != 0:
-                self.__b = 0
-                self.__c = (plane1.a * plane2.d - plane2.a * plane1.d) / val2_2
-                self.__a = -(plane2.c * self.__c + plane2.d) / plane2.a
+                self._b = 0
+                self._c = (plane1.a * plane2.d - plane2.a * plane1.d) / val2_2
+                self._a = -(plane2.c * self._c + plane2.d) / plane2.a
 
             elif val3_1 != 0 and plane2.c != 0:
-                self.__a = 0
-                self.__b = (plane1.c * plane2.d - plane1.d * plane2.c) / val3_1
-                self.__c = -(plane2.b * self.__b + plane2.d) / plane2.c
+                self._a = 0
+                self._b = (plane1.c * plane2.d - plane1.d * plane2.c) / val3_1
+                self._c = -(plane2.b * self._b + plane2.d) / plane2.c
             elif val3_2 != 0 and plane2.b != 0:
-                self.__a = 0
-                self.__c = (plane2.b * plane1.d - plane2.d * plane1.b) / val3_2
-                self.__b = -(plane2.c * self.__c + plane2.d) / plane2.b
+                self._a = 0
+                self._c = (plane2.b * plane1.d - plane2.d * plane1.b) / val3_2
+                self._b = -(plane2.c * self._c + plane2.d) / plane2.b
             else:
                 logger.debug("Zero Error")
         else:
@@ -231,11 +231,14 @@ class Line:
 class Line_segment(Line):
     def __init__(self, a=0, b=0, c=0, p1=1, p2=0, p3=0, point1=None, point2=None):
         if point1 is not None and point2 is not None:
+            super().__init__(a, b, c, p1, p2, p3)
             self.segment_create_from_points(point1, point2)
         else:
             point1 = np.array([0, 0, 0])
             point2 = np.array([1, 0, 0])
-        super().__init__(a, b, c, p1, p2, p3)
+            super().__init__(a, b, c, p1, p2, p3)
+        self.color = 'blue'
+        self.linewidth = 2
         self.point1 = np.array(point1)
         self.point2 = np.array(point2)
         # Сортировка по возрастанию для удобства дальнейшей работы
@@ -247,62 +250,61 @@ class Line_segment(Line):
         self.border_z.sort()
         self.lenth = np.linalg.norm(self.point1 - self.point2)
 
-    @property
-    def a(self):
-        logger.debug(self.__a)
-        return self.__a
-
-    @property
-    def b(self):
-        return self.__b
-
-    @property
-    def c(self):
-        return self.__c
-
-    @property
-    def p1(self):
-        return self.__p1
-
-    @property
-    def p2(self):
-        return self.__p2
-
-    @property
-    def p3(self):
-        return self.__p3
-
-    @a.setter
-    def a(self, a):
-        self.__a = a
-
-    @b.setter
-    def b(self, b):
-        self.__b = b
-
-    @c.setter
-    def c(self, c):
-        self.__c = c
-
-    @p1.setter
-    def p1(self, p1):
-        self.__p1 = p1
-
-    @p2.setter
-    def p2(self, p2):
-        self.__p2 = p2
-
-    @p3.setter
-    def p3(self, p3):
-        self.__p3 = p3
+    # @property
+    # def a(self):
+    #     return self.__a
+    #
+    # @property
+    # def b(self):
+    #     return self.__b
+    #
+    # @property
+    # def c(self):
+    #     return self.__c
+    #
+    # @property
+    # def p1(self):
+    #     return self.__p1
+    #
+    # @property
+    # def p2(self):
+    #     return self.__p2
+    #
+    # @property
+    # def p3(self):
+    #     return self.__p3
+    #
+    # @a.setter
+    # def a(self, a):
+    #     self.__a = a
+    #
+    # @b.setter
+    # def b(self, b):
+    #     self.__b = b
+    #
+    # @c.setter
+    # def c(self, c):
+    #     self.__c = c
+    #
+    # @p1.setter
+    # def p1(self, p1):
+    #     self.__p1 = p1
+    #
+    # @p2.setter
+    # def p2(self, p2):
+    #     self.__p2 = p2
+    #
+    # @p3.setter
+    # def p3(self, p3):
+    #     self.__p3 = p3
 
     def info(self) -> None:
         logger.debug(
-            f'a = {self.__a}, b = {self.__b}, c = {self.__c}, p1 = {self.__p1}, p2 = {self.__p2}, p3 = {self.__p3}'
+            f'a = {self._a}, b = {self._b}, c = {self._c}, p1 = {self._p1}, p2 = {self._p2}, p3 = {self._p3}'
             f'\npoint1 = {self.point1}, point2 = {self.point2}')
 
     def coeffs(self) -> ndarray:
-        return np.array([self.__a, self.__b, self.__c, self.__p1, self.__p2, self.__p3])
+        return np.array([self._a, self._b, self._c, self._p1, self._p2, self._p3])
 
     def segment_create_from_points(self, point1, point2) -> None:
         """
@@ -313,13 +315,13 @@ class Line_segment(Line):
         :return: None
         """
         if np.shape(point1)[0] == 2:
-            self.__c = 0
+            self._c = 0
             p3 = 0
         else:
-            self.__c = point1[2]
+            self._c = point1[2]
             p3 = point2[2] - point1[2]
-        self.__a = point1[0]
-        self.__b = point1[1]
+        self._a = point1[0]
+        self._b = point1[1]
 
         # TODO: проверка на нуль
         p1 = point2[0] - point1[0]
@@ -383,7 +385,7 @@ class Line_segment(Line):
         return np.vstack([self.point1, self.point2])
     def show(self, ax):
         vT = self.get_points().T
-        ax.plot(vT[0], vT[1], vT[2], 'm')
+        ax.plot(vT[0], vT[1], vT[2], 'm', color=self.color, linewidth=self.linewidth)
     # def lsftp(self, triangle, plane):
     #     '''
     #      Line segment from triangle and plane или сокращенно lsftp
